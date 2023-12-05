b0df1e7a04f1648c90e78026c1c1dfdd0992a694
okelloEnos <enosokello@gmail.com>
2023-12-05 18:53:01 +0300

    dependencies and resource setup

---
+++ lib/resources/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color primaryColor = Color(0xFFADFF2F);
+  static const Color secondaryColor = Color(0xFFF2F2F2);
+  static const Color tertiaryColor = Color(0xFF0A0A0A);
+}
