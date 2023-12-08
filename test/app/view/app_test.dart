c03594c0aeb0e9f950595ee8765042a4099d90b0
okelloEnos <enosokello@gmail.com>
2023-12-08 13:39:02 +0300

    add one test case

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:onspace/app/app.dart';
 import 'package:onspace/features/home/bloc/bottom_navigation_bar_cubit/bottom_navigation_bar_cubit.dart';
 import 'package:onspace/features/home/bloc/bottom_navigation_bar_cubit/bottom_navigation_history_cubit.dart';
 import 'package:onspace/features/home/ui/screens/home_screen.dart';
